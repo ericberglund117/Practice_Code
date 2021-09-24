860a4330b7e0542adf04e843c3df4b6563ec8388
Eric Berglund <ericberglund117@gmail.com>
2021-09-24 15:26:41 -0600

    Add nobel data

---
+++ src/Nobels.js
@@ -0,0 +1,211 @@
+const laureates2017 = [
+    {
+      "id": "938",
+      "firstname": "Jeffrey C.",
+      "surname": "Hall",
+      "born": "1945-05-03",
+      "died": "0000-00-00",
+      "bornCountry": "USA",
+      "bornCountryCode": "US",
+      "bornCity": "New York, NY",
+    },
+    {
+      "id": "939",
+      "firstname": "Michael",
+      "surname": "Rosbash",
+      "born": "1944-03-07",
+      "died": "0000-00-00",
+      "bornCountry": "USA",
+      "bornCountryCode": "US",
+      "bornCity": "Kansas City, MO",
+    },
+    {
+      "id": "940",
+      "firstname": "Michael W.",
+      "surname": "Young",
+      "born": "1949-03-28",
+      "died": "0000-00-00",
+      "bornCountry": "USA",
+      "bornCountryCode": "US",
+      "bornCity": "Miami, FL",
+    },
+    {
+      "id": "941",
+      "firstname": "Rainer",
+      "surname": "Weiss",
+      "born": "1932-09-29",
+      "died": "0000-00-00",
+      "bornCountry": "Germany",
+      "bornCountryCode": "DE",
+      "bornCity": "Berlin",
+    },
+    {
+      "id": "942",
+      "firstname": "Barry C.",
+      "surname": "Barish",
+      "born": "1936-01-27",
+      "died": "0000-00-00",
+      "bornCountry": "USA",
+      "bornCountryCode": "US",
+      "bornCity": "Omaha, NE",
+    },
+    {
+      "id": "943",
+      "firstname": "Kip S.",
+      "surname": "Thorne",
+      "born": "1940-06-01",
+      "died": "0000-00-00",
+      "bornCountry": "USA",
+      "bornCountryCode": "US",
+      "bornCity": "Logan, UT",
+    },
+    {
+      "id": "944",
+      "firstname": "Jacques",
+      "surname": "Dubochet",
+      "born": "1942-06-08",
+      "died": "0000-00-00",
+      "bornCountry": "Switzerland",
+      "bornCountryCode": "CH",
+      "bornCity": "Aigle",
+    },
+    {
+      "id": "945",
+      "firstname": "Joachim",
+      "surname": "Frank",
+      "born": "1940-09-12",
+      "died": "0000-00-00",
+      "bornCountry": "Germany",
+      "bornCountryCode": "DE",
+      "bornCity": "Siegen",
+    },
+    {
+      "id": "946",
+      "firstname": "Richard",
+      "surname": "Henderson",
+      "born": "1945-07-19",
+      "died": "0000-00-00",
+      "bornCountry": "Scotland",
+      "bornCountryCode": "GB",
+      "bornCity": "Edinburgh",
+    },
+    {
+      "id": "947",
+      "firstname": "Kazuo",
+      "surname": "Ishiguro",
+      "born": "1954-11-08",
+      "died": "0000-00-00",
+      "bornCountry": "Japan",
+      "bornCountryCode": "JP",
+      "bornCity": "Nagasaki",
+    },
+    {
+      "id": "949",
+      "firstname": "Richard H.",
+      "surname": "Thaler",
+      "born": "1945-09-12",
+      "died": "0000-00-00",
+      "bornCountry": "USA",
+      "bornCountryCode": "US",
+      "bornCity": "East Orange, NJ",
+    }
+  ]
+  
+  const prizes2017 = [
+    {
+      "year": "2017",
+      "category": "physics",
+      "laureates": [
+        {
+          "id": "941",
+          "firstname": "Rainer",
+          "surname": "Weiss",
+          "share": "2"
+        },
+        {
+          "id": "942",
+          "firstname": "Barry C.",
+          "surname": "Barish",
+          "share": "4"
+        },
+        {
+          "id": "943",
+          "firstname": "Kip S.",
+          "surname": "Thorne",
+          "share": "4"
+        }
+      ]
+    },
+    {
+      "year": "2017",
+      "category": "chemistry",
+      "laureates": [
+        {
+          "id": "944",
+          "firstname": "Jacques",
+          "surname": "Dubochet",
+          "share": "3"
+        },
+        {
+          "id": "945",
+          "firstname": "Joachim",
+          "surname": "Frank",
+          "share": "3"
+        },
+        {
+          "id": "946",
+          "firstname": "Richard",
+          "surname": "Henderson",
+          "share": "3"
+        }
+      ]
+    },
+    {
+      "year": "2017",
+      "category": "medicine",
+      "laureates": [
+        {
+          "id": "938",
+          "firstname": "Jeffrey C.",
+          "surname": "Hall",
+          "share": "3"
+        },
+        {
+          "id": "939",
+          "firstname": "Michael",
+          "surname": "Rosbash",
+          "share": "3"
+        },
+        {
+          "id": "940",
+          "firstname": "Michael W.",
+          "surname": "Young",
+          "share": "3"
+        }
+      ]
+    },
+    {
+      "year": "2017",
+      "category": "literature",
+      "laureates": [
+        {
+          "id": "947",
+          "firstname": "Kazuo",
+          "surname": "Ishiguro",
+          "share": "1"
+        }
+      ]
+    },
+    {
+      "year": "2017",
+      "category": "economics",
+      "laureates": [
+        {
+          "id": "949",
+          "firstname": "Richard H.",
+          "surname": "Thaler",
+          "share": "1"
+        }
+      ]
+    }
+  ]
