8d64fee67547f2a4451fac6d8a2aaf6b90cbd41e
Eric Berglund <ericberglund117@gmail.com>
2021-09-27 15:50:26 -0600

    Solve problem 3

--- src/DenverRest.js
+++ src/DenverRest.js
@@ -21,6 +21,15 @@ const restaurants = [
   console.log(totalReviews)
 
   //2. Create an object where each property is the name of a restaurant and its key value is its number of reviews.
+  //*EXPECTED OUTPUT*//
+  //{ 'Fruition Restaurant': 788,
+  // 'Sushi Den': 479,
+  // 'Sam\'s No. 3': 1870,
+  // 'Pete\'s Kitchen': 236,
+  // 'Root Down': 972,
+  // 'The Capital Grille': 531,
+  // Acorn: 309,
+  // Panzano: 1316 }
 const restObj = restaurants.reduce((acc, rest) => {
     if(!acc[rest.name]) {
       acc[rest.name] = rest.number_of_reviews
@@ -28,3 +37,19 @@ const restObj = restaurants.reduce((acc, rest) => {
     return acc
   }, {})
   console.log(restObj)
+
+  // 3. Make a new object where each key is a type of restaurant and its value is an array of the matching restaurants
+    //*EXPECTED OUTPUT*//
+//  { Fine Dining: [ 'Fruition Restaurant', 'Fruition Restaurant', 'Sushi Den' ],
+//   Cheap Eats: [ 'Sam\'s No. 3', 'Sam\'s No. 3', 'Pete\'s Kitchen' ],
+//   Lunch: [ 'Root Down', 'Root Down', 'The Capital Grille' ],
+//   Dinner: [ 'Acorn', 'Acorn', 'Panzano' ] }
+const restKey = restaurants.reduce((acc, rest) => {
+    if(!acc[rest.type]) {
+      acc[rest.type] = [rest.name]
+    } else {
+      acc[rest.type].push(rest.name)
+    }
+    return acc
+  }, {})
+  console.log(restKey)
