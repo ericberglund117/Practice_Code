5b23fac0e44f84210331d7233c7a2a43582988de
Eric Berglund <ericberglund117@gmail.com>
2021-09-27 11:46:22 -0600

    Add philosopher data

---
+++ src/Philosophers.js
@@ -0,0 +1,39 @@
+const philosophers = [
+    {
+      name: 'Georg Wilhelm Friedrich Hegel',
+      born: 1770,
+      died: 1831,
+      school: 'Continental Philosophy',
+      main_interests: ['Metaphysics', 'Epistemology', 'Logic', 'History of Philosophy', 'Political Philosophy', 'Aesthetics']
+    },
+    { 
+      name: 'Bertrand Russell',
+      born: 1872,
+      died: 1970,
+      school: 'Analytic Philosophy',
+      main_interests: ['Logic', 'Ontology', 'Epistemology', 'Ethics', 'Mathematics', 'Philosophy of Mind', 'Philosophy of Religion', 'Philosophy of Language', 'Philosophy of Mathematics', 'Philosophy of Science', 'Set Theory']
+    },
+    { 
+      name: 'David Lewis',
+      born: 1941,
+      died: 2001,
+      school: 'Analytic Philosophy',
+      main_interests: ['Logic', 'Language', 'Epistemology', 'Metaphysics', 'Ethics'],
+    },
+    { 
+      name: 'Soren Keirkegaard',
+      born: 1813,
+      died: 1855,
+      school: 'Continental Philosophy',
+      main_interests: ['Christianity','Epistemology', 'Ethics', 'Metaphysics',
+      'Philosophy of Christianity','Poetry',
+      'Psychology']
+    },
+    { 
+      name: 'William Van Orman Quine',
+      born: 1908,
+      died: 2000,
+      school: 'Analytic Philosophy',
+      main_interests: ['Logic', 'Ontology', 'Epistemology', 'Philosophy of Language', 'Philosophy of Mathematics', 'Philosophy of Science', 'Set Theory']
+    }
+  ]
