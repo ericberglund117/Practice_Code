bec5bcf0718842c122597aaccb6a1b828c062137
Eric Berglund <ericberglund117@gmail.com>
2021-09-27 15:11:11 -0600

    Solve problem 1

---
+++ src/Phish.js
@@ -0,0 +1,512 @@
+const showData = [
+    {
+      id: 1903,
+      date: "2017-01-13",
+      duration: 10340152,
+      incomplete: false,
+      missing: false,
+      tour_id: 100,
+      venue: {
+        id: 817,
+        name: "Barcelo Maya Beach",
+        past_names: null,
+        latitude: 20.631389,
+        longitude: -87.073056,
+        shows_count: 3,
+        location: "Riviera Maya, Quintana Roo, Mexico",
+        img: 'https://www.jambase.com/wp-content/uploads/2016/09/phish-riviera-maya-2017-glory-1480x832.jpg'
+      }
+    },
+    {
+      id: 1904,
+      date: "2017-01-14",
+      duration: 10835436,
+      incomplete: false,
+      missing: false,
+      tour_id: 100,
+      venue: {
+        id: 817,
+        name: "Barcelo Maya Beach",
+        past_names: null,
+        latitude: 20.631389,
+        longitude: -87.073056,
+        shows_count: 3,
+        location: "Riviera Maya, Quintana Roo, Mexico",
+        img: 'https://www.jambase.com/wp-content/uploads/2016/09/phish-riviera-maya-2017-glory-1480x832.jpg'
+      }
+    },
+    {
+      id: 1905,
+      date: "2017-01-15",
+      duration: 10690062,
+      incomplete: false,
+      missing: false,
+      tour_id: 100,
+      venue: {
+        id: 817,
+        name: "Barcelo Maya Beach",
+        past_names: null,
+        latitude: 20.631389,
+        longitude: -87.073056,
+        shows_count: 3,
+        location: "Riviera Maya, Quintana Roo, Mexico",
+        img: 'https://www.jambase.com/wp-content/uploads/2016/09/phish-riviera-maya-2017-glory-1480x832.jpg'
+      }
+    },
+    {
+      id: 1906,
+      date: "2017-07-14",
+      duration: 9542453,
+      incomplete: false,
+      missing: false,
+      tour_id: 101,
+      venue: {
+        id: 768,
+        name: "Huntington Bank Pavilion at Northerly Island",
+        past_names: "Charter One Pavilion, FirstMerit Bank Pavilion",
+        latitude: 41.867516,
+        longitude: -87.609299,
+        shows_count: 9,
+        location: "Chicago, IL",
+        img: 'https://liveforlivemusic.com/wp-content/uploads/2017/07/Phish-Northernly-Island-Chicago-Ill.-July-14-2017_PRC2519-Edit.jpg'
+      }
+    },
+    { 
+      id: 1908,
+      date: '2017-07-15',
+      duration: 9375006,
+      incomplete: false,
+      missing: false,
+      tour_id: 101,
+      venue: { 
+        id: 768,
+        name: 'Huntington Bank Pavilion at Northerly Island',
+        past_names: 'Charter One Pavilion, FirstMerit Bank Pavilion',
+        latitude: 41.867516,
+        longitude: -87.609299,
+        shows_count: 9,
+        location: 'Chicago, IL',
+        img: 'https://liveforlivemusic.com/wp-content/uploads/2017/07/Phish-Northernly-Island-Chicago-Ill.-July-14-2017_PRC2519-Edit.jpg'
+      } 
+    },
+    { 
+      id: 1909,
+      date: '2017-07-16',
+      duration: 9545691,
+      incomplete: false,
+      missing: false,
+      tour_id: 101,
+      venue: { 
+        id: 768,
+        name: 'Huntington Bank Pavilion at Northerly Island',
+        past_names: 'Charter One Pavilion, FirstMerit Bank Pavilion',
+        latitude: 41.867516,
+        longitude: -87.609299,
+        shows_count: 9,
+        location: 'Chicago, IL',
+        img: 'https://liveforlivemusic.com/wp-content/uploads/2017/07/Phish-Northernly-Island-Chicago-Ill.-July-14-2017_PRC2519-Edit.jpg'
+      } 
+    },
+    { 
+      id: 1910,
+      date: '2017-07-18',
+      duration: 9741273,
+      incomplete: false,
+      missing: false,
+      tour_id: 101,
+      venue: { 
+        id: 241,
+        name: 'Ervin J. Nutter Center, Wright State University',
+        past_names: null,
+        latitude: 39.7804794,
+        longitude: -84.0525888,
+        shows_count: 3,
+        location: 'Dayton, OH',
+        img: 'https://2.bp.blogspot.com/-dlnlW1e34qE/WW77G8B0hsI/AAAAAAAAIrI/EjOeG3IDDksBNkGOIi7bD4fgW00VfnxvACLcBGAs/s1600/07-18-17.jpg'
+      } 
+    },
+    { 
+      id: 1911,
+      date: '2017-07-19',
+      duration: 9731030,
+      incomplete: false,
+      missing: false,
+      tour_id: 101,
+      venue: { 
+        id: 818,
+        name: 'Petersen Events Center',
+        past_names: null,
+        latitude: 40.443777,
+        longitude: -79.962274,
+        shows_count: 1,
+        location: 'Pittsburgh, PA',
+        img: 'https://2ab9pu2w8o9xpg6w26xnz04d-wpengine.netdna-ssl.com/wp-content/uploads/2017/07/yinzgram-1200x609.jpg' 
+      } 
+    },
+    { 
+      id: 1916,
+      date: '2017-07-21',
+      duration: 9379815,
+      incomplete: false,
+      missing: false,
+      tour_id: 101,
+      venue: { 
+        id: 408,
+        name: 'Madison Square Garden',
+        past_names: null,
+        latitude: 40.7505045,
+        longitude: -73.9934387,
+        shows_count: 56,
+        location: 'New York, NY',
+        img: 'https://1.bp.blogspot.com/-syPbUr0w3N4/WXmCFrhXoAI/AAAAAAAAI5M/4hjcaLQQ8joRtT_c3YacRdCnMcfkD3KMACLcBGAs/s1600/07-26-17.1.jpg' 
+      } 
+    },
+    { 
+      id: 1912,
+      date: '2017-07-22',
+      duration: 9895288,
+      incomplete: false,
+      missing: false,
+      tour_id: 101,
+      venue: { 
+        id: 408,
+        name: 'Madison Square Garden',
+        past_names: null,
+        latitude: 40.7505045,
+        longitude: -73.9934387,
+        shows_count: 56,
+        location: 'New York, NY',
+        img: 'https://1.bp.blogspot.com/-syPbUr0w3N4/WXmCFrhXoAI/AAAAAAAAI5M/4hjcaLQQ8joRtT_c3YacRdCnMcfkD3KMACLcBGAs/s1600/07-26-17.1.jpg' 
+      } 
+    },
+    { 
+      id: 1913,
+      date: '2017-07-23',
+      duration: 9000518,
+      incomplete: false,
+      missing: false,
+      tour_id: 101,
+      venue: { 
+        id: 408,
+        name: 'Madison Square Garden',
+        past_names: null,
+        latitude: 40.7505045,
+        longitude: -73.9934387,
+        shows_count: 56,
+        location: 'New York, NY',
+        img: 'https://1.bp.blogspot.com/-syPbUr0w3N4/WXmCFrhXoAI/AAAAAAAAI5M/4hjcaLQQ8joRtT_c3YacRdCnMcfkD3KMACLcBGAs/s1600/07-26-17.1.jpg' 
+      } 
+    },
+    { 
+      id: 1914,
+      date: '2017-07-25',
+      duration: 10016040,
+      incomplete: false,
+      missing: false,
+      tour_id: 101,
+      venue: { 
+        id: 408,
+        name: 'Madison Square Garden',
+        past_names: null,
+        latitude: 40.7505045,
+        longitude: -73.9934387,
+        shows_count: 56,
+        location: 'New York, NY',
+        img: 'https://1.bp.blogspot.com/-syPbUr0w3N4/WXmCFrhXoAI/AAAAAAAAI5M/4hjcaLQQ8joRtT_c3YacRdCnMcfkD3KMACLcBGAs/s1600/07-26-17.1.jpg' 
+      } 
+    },
+    { 
+      id: 1917,
+      date: '2017-07-26',
+      duration: 9767238,
+      incomplete: false,
+      missing: false,
+      tour_id: 101,
+      venue: { 
+        id: 408,
+        name: 'Madison Square Garden',
+        past_names: null,
+        latitude: 40.7505045,
+        longitude: -73.9934387,
+        shows_count: 56,
+        location: 'New York, NY',
+        img: 'https://1.bp.blogspot.com/-syPbUr0w3N4/WXmCFrhXoAI/AAAAAAAAI5M/4hjcaLQQ8joRtT_c3YacRdCnMcfkD3KMACLcBGAs/s1600/07-26-17.1.jpg' 
+      } 
+    },
+    { 
+      id: 1918,
+      date: '2017-07-28',
+      duration: 9852684,
+      incomplete: false,
+      missing: false,
+      tour_id: 101,
+      venue: { 
+        id: 408,
+        name: 'Madison Square Garden',
+        past_names: null,
+        latitude: 40.7505045,
+        longitude: -73.9934387,
+        shows_count: 56,
+        location: 'New York, NY',
+        img: 'https://1.bp.blogspot.com/-syPbUr0w3N4/WXmCFrhXoAI/AAAAAAAAI5M/4hjcaLQQ8joRtT_c3YacRdCnMcfkD3KMACLcBGAs/s1600/07-26-17.1.jpg' 
+      } 
+    },
+    { 
+      id: 1919,
+      date: '2017-07-29',
+      duration: 9546711,
+      incomplete: false,
+      missing: false,
+      tour_id: 101,
+      venue: { 
+        id: 408,
+        name: 'Madison Square Garden',
+        past_names: null,
+        latitude: 40.7505045,
+        longitude: -73.9934387,
+        shows_count: 56,
+        location: 'New York, NY',
+        img: 'https://1.bp.blogspot.com/-syPbUr0w3N4/WXmCFrhXoAI/AAAAAAAAI5M/4hjcaLQQ8joRtT_c3YacRdCnMcfkD3KMACLcBGAs/s1600/07-26-17.1.jpg' 
+      } 
+    },
+    { 
+      id: 1920,
+      date: '2017-07-30',
+      duration: 9759504,
+      incomplete: false,
+      missing: false,
+      tour_id: 101,
+      venue: { 
+        id: 408,
+        name: 'Madison Square Garden',
+        past_names: null,
+        latitude: 40.7505045,
+        longitude: -73.9934387,
+        shows_count: 56,
+        location: 'New York, NY',
+        img: 'https://1.bp.blogspot.com/-syPbUr0w3N4/WXmCFrhXoAI/AAAAAAAAI5M/4hjcaLQQ8joRtT_c3YacRdCnMcfkD3KMACLcBGAs/s1600/07-26-17.1.jpg' 
+      } 
+    },
+    { 
+      id: 1921,
+      date: '2017-08-01',
+      duration: 9137267,
+      incomplete: false,
+      missing: false,
+      tour_id: 101,
+      venue: { 
+        id: 408,
+        name: 'Madison Square Garden',
+        past_names: null,
+        latitude: 40.7505045,
+        longitude: -73.9934387,
+        shows_count: 56,
+        location: 'New York, NY',
+        img: 'https://1.bp.blogspot.com/-syPbUr0w3N4/WXmCFrhXoAI/AAAAAAAAI5M/4hjcaLQQ8joRtT_c3YacRdCnMcfkD3KMACLcBGAs/s1600/07-26-17.1.jpg' 
+      } 
+    },
+    { 
+      id: 1922,
+      date: '2017-08-02',
+      duration: 9981700,
+      incomplete: false,
+      missing: false,
+      tour_id: 101,
+      venue: { 
+        id: 408,
+        name: 'Madison Square Garden',
+        past_names: null,
+        latitude: 40.7505045,
+        longitude: -73.9934387,
+        shows_count: 56,
+        location: 'New York, NY',
+        img: 'https://1.bp.blogspot.com/-syPbUr0w3N4/WXmCFrhXoAI/AAAAAAAAI5M/4hjcaLQQ8joRtT_c3YacRdCnMcfkD3KMACLcBGAs/s1600/07-26-17.1.jpg' 
+      } 
+    },
+    { 
+      id: 1923,
+      date: '2017-08-04',
+      duration: 9098553,
+      incomplete: false,
+      missing: false,
+      tour_id: 101,
+      venue: { 
+        id: 408,
+        name: 'Madison Square Garden',
+        past_names: null,
+        latitude: 40.7505045,
+        longitude: -73.9934387,
+        shows_count: 56,
+        location: 'New York, NY',
+        img: 'https://1.bp.blogspot.com/-syPbUr0w3N4/WXmCFrhXoAI/AAAAAAAAI5M/4hjcaLQQ8joRtT_c3YacRdCnMcfkD3KMACLcBGAs/s1600/07-26-17.1.jpg' 
+      } 
+    },
+    { 
+      id: 1924,
+      date: '2017-08-05',
+      duration: 9693805,
+      incomplete: false,
+      missing: false,
+      tour_id: 101,
+      venue: { 
+        id: 408,
+        name: 'Madison Square Garden',
+        past_names: null,
+        latitude: 40.7505045,
+        longitude: -73.9934387,
+        shows_count: 56,
+        location: 'New York, NY',
+        img: 'https://1.bp.blogspot.com/-syPbUr0w3N4/WXmCFrhXoAI/AAAAAAAAI5M/4hjcaLQQ8joRtT_c3YacRdCnMcfkD3KMACLcBGAs/s1600/07-26-17.1.jpg'
+      } 
+    },
+    { 
+      id: 1925,
+      date: '2017-08-06',
+      duration: 9002003,
+      incomplete: false,
+      missing: false,
+      tour_id: 101,
+      venue: { 
+        id: 408,
+        name: 'Madison Square Garden',
+        past_names: null,
+        latitude: 40.7505045,
+        longitude: -73.9934387,
+        shows_count: 56,
+        location: 'New York, NY',
+        img: 'https://1.bp.blogspot.com/-syPbUr0w3N4/WXmCFrhXoAI/AAAAAAAAI5M/4hjcaLQQ8joRtT_c3YacRdCnMcfkD3KMACLcBGAs/s1600/07-26-17.1.jpg' 
+      } 
+    },
+    { 
+      id: 1926,
+      date: '2017-09-01',
+      duration: 10223828,
+      incomplete: false,
+      missing: false,
+      tour_id: 101,
+      venue: { 
+        id: 207,
+        name: 'Dick\'s Sporting Goods Park',
+        past_names: null,
+        latitude: 39.8058438,
+        longitude: -104.8918854,
+        shows_count: 24,
+        location: 'Commerce City, CO',
+        img: 'http://coloradist.com/wpcoloradist/wp-content/uploads/2017/09/2017-09-01-phish-dicks-commerce-city-co-19.jpg' 
+      } 
+    },
+    { 
+      id: 1927,
+      date: '2017-09-02',
+      duration: 11215544,
+      incomplete: false,
+      missing: false,
+      tour_id: 101,
+      venue: { 
+        id: 207,
+        name: 'Dick\'s Sporting Goods Park',
+        past_names: null,
+        latitude: 39.8058438,
+        longitude: -104.8918854,
+        shows_count: 24,
+        location: 'Commerce City, CO',
+        img: 'http://coloradist.com/wpcoloradist/wp-content/uploads/2017/09/2017-09-01-phish-dicks-commerce-city-co-19.jpg' 
+      } 
+    },
+    { 
+      id: 1928,
+      date: '2017-09-03',
+      duration: 10271634,
+      incomplete: false,
+      missing: false,
+      tour_id: 101,
+      venue: { 
+        id: 207,
+        name: 'Dick\'s Sporting Goods Park',
+        past_names: null,
+        latitude: 39.8058438,
+        longitude: -104.8918854,
+        shows_count: 24,
+        location: 'Commerce City, CO',
+        img: 'http://coloradist.com/wpcoloradist/wp-content/uploads/2017/09/2017-09-01-phish-dicks-commerce-city-co-19.jpg' 
+      } 
+    },
+    { 
+      id: 1929,
+      date: '2017-12-28',
+      duration: 10342656,
+      incomplete: false,
+      missing: false,
+      tour_id: 102,
+      venue: { 
+        id: 408,
+        name: 'Madison Square Garden',
+        past_names: null,
+        latitude: 40.7505045,
+        longitude: -73.9934387,
+        shows_count: 56,
+        location: 'New York, NY',
+        img: 'http://www.brooklynvegan.com/files/img/as/phish-msg-2015.jpg' 
+      } 
+    },
+    { 
+      id: 1930,
+      date: '2017-12-29',
+      duration: 9504914,
+      incomplete: false,
+      missing: false,
+      tour_id: 102,
+      venue: { 
+        id: 408,
+        name: 'Madison Square Garden',
+        past_names: null,
+        latitude: 40.7505045,
+        longitude: -73.9934387,
+        shows_count: 56,
+        location: 'New York, NY',
+        img: 'http://www.brooklynvegan.com/files/img/as/phish-msg-2015.jpg' 
+      } 
+    },
+    { 
+      id: 1931,
+      date: '2017-12-30',
+      duration: 9342719,
+      incomplete: false,
+      missing: false,
+      tour_id: 102,
+      venue: { 
+        id: 408,
+        name: 'Madison Square Garden',
+        past_names: null,
+        latitude: 40.7505045,
+        longitude: -73.9934387,
+        shows_count: 56,
+        location: 'New York, NY',
+        img: 'http://www.brooklynvegan.com/files/img/as/phish-msg-2015.jpg' 
+      } 
+    },
+    { 
+      id: 1932,
+      date: '2017-12-31',
+      duration: 13698664,
+      incomplete: false,
+      missing: false,
+      tour_id: 102,
+      venue: { 
+        id: 408,
+        name: 'Madison Square Garden',
+        past_names: null,
+        latitude: 40.7505045,
+        longitude: -73.9934387,
+        shows_count: 56,
+        location: 'New York, NY',
+        img: 'http://www.brooklynvegan.com/files/img/as/phish-msg-2015.jpg' 
+      } 
+    }
+  ]
+
+  // Create a list of all of the tour dates
+    const tourDates = () => {
+        return showData.map(show => show.date)
+    }
+    tourDates()
