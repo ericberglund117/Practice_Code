faf94cf5a01b9b6ba5b96ea0abd0b2449ceb03c0
Eric Berglund <ericberglund117@gmail.com>
2021-09-27 15:47:50 -0600

    Add denver rest data

---
+++ src/DenverRest.js
@@ -0,0 +1,10 @@
+const restaurants = [
+    { name: "Fruition Restaurant", type: "Fine Dining", number_of_reviews: 788, neighborhood: "Cap Hill" },
+    { name: "Sushi Den", type: "Fine Dining", number_of_reviews: 479, neighborhood: "Platt Park" },
+    { name: "Sam's No. 3", type: "Cheap Eats", number_of_reviews: 1870, neighborhood: "LoDo" },
+    { name: "Pete's Kitchen", type: "Cheap Eats", number_of_reviews: 236, neighborhood: "City Park" },
+    { name: "Root Down", type: "Lunch", number_of_reviews: 972, neighborhood: "Highlands" },
+    { name: "The Capital Grille", type: "Lunch", number_of_reviews: 531, neighborhood: "LoDo" },
+    { name: "Acorn", type: "Dinner", number_of_reviews: 309, neighborhood: "RiNo" },
+    { name: "Panzano", type: "Dinner", number_of_reviews: 1316, neighborhood: "LoDo" }
+]
