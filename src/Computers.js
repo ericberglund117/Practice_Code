ce18f363ea4ead61bf72c8b15575a0ad2f91e2cb
Eric Berglund <ericberglund117@gmail.com>
2021-10-01 17:45:35 -0600

    Solve problem 1

--- src/Computers.js
+++ src/Computers.js
@@ -44,3 +44,37 @@ const computers = [
       accessories: ['mouse', 'keyboard', 'rgb', 'microphone']
     }
   ]
+
+  // Below, return an array of objects that are components for each computer: Answer:
+
+// [
+//   {
+//     gpu: 'Nvidia Geoforce GTX 1080ti',
+//     ram: '32GB',
+//     cpu: 'Intel Core i7'
+//   },
+//   {
+//     gpu: 'Radeon Pro Vega 20',
+//     ram: '32GB',
+//     cpu: 'Intel Core i9'
+//   },
+//   {
+//     gpu: 'NVIDIA Geforce RTX 2080',
+//     ram: '16GB',
+//     cpu: 'Intel Core i9'
+//   },
+//   {
+//     gpu: 'Radeon Vega 10',
+//     ram: '8GB',
+//     cpu: 'Intel Core i5'
+//   },
+// ]
+
+const compoOnly = () => {
+    return computers.reduce((acc, comp) => {
+      acc.push(comp.components)
+      return acc
+    },[])
+  }
+  
+  compoOnly()
