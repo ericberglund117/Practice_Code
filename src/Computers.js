ecbaa680193406fd9760d2d66bcd0d40a2276f8f
Eric Berglund <ericberglund117@gmail.com>
2021-10-01 17:40:20 -0600

    Add computers data

---
+++ src/Computers.js
@@ -0,0 +1,46 @@
+const computers = [
+    {
+      cost: 2000,
+      components: {
+        gpu: 'Nvidia Geoforce GTX 1080ti',
+        ram: '32GB',
+        cpu: 'Intel Core i7'
+      },
+      isNew: false,
+      isLaptop: false,
+      accessories: ['mouse', 'keyboard', 'headset', 'rgb', 'darthVader']
+    },
+    {
+      cost: 1200,
+      components: {
+        gpu: 'Radeon Pro Vega 20',
+        ram: '32GB',
+        cpu: 'Intel Core i9'
+      },
+      isNew: true,
+      isLaptop: true,
+      accessories: ['mouse', 'keyboard', 'liquidCooling', 'headset']
+    },
+    {
+      cost: 2800,
+      components: {
+        gpu: 'NVIDIA Geforce RTX 2080',
+        ram: '16GB',
+        cpu: 'Intel Core i9'
+      },
+      isNew: true,
+      isLaptop: false,
+      accessories: ['mouse', 'keyboard', 'rgb']
+    },
+    {
+      cost: 800,
+      components: {
+        gpu: 'Radeon Vega 10',
+        ram: '8GB',
+        cpu: 'Intel Core i5'
+      },
+      isNew: true,
+      isLaptop: false,
+      accessories: ['mouse', 'keyboard', 'rgb', 'microphone']
+    }
+  ]
