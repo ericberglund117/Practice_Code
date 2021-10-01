b97331856be3ca2d0bb046ad006a0bef0c4303b3
Eric Berglund <ericberglund117@gmail.com>
2021-10-01 17:26:20 -0600

    Add orders data

---
+++ src/Orders.js
@@ -0,0 +1,36 @@
+const orders = [
+    {
+      type: 'cake',
+      name: 'Strawberries & Cream Cake',
+      price: 40,
+      size: 6,
+      customer: 'Olivia',
+      specialConsiderations: 'gluten free*pick up on Thursday afternoon*birthday cake'
+    },
+    {
+      type: 'tart',
+      name: 'Chocolate Raspberry Tart',
+      price: 25,
+      size: 8,
+      customer: 'Cody',
+      specialConsiderations: 'pick up on Friday*no allergies'
+    },
+    {
+      type: 'cake',
+      name: 'Chocolate Cake',
+      price: 40,
+      size: 6,
+      customer: 'Ramiro',
+      specialConsiderations: 'olive oil allergy*add cream cheese icing*simple decoration'
+    },
+    {
+      type: 'cake',
+      name: 'Funfetti Cake',
+      price: 0,
+      size: 6,
+      customer: 'Leta',
+      specialConsiderations: 'none'
+    }
+  ];
+
+  
